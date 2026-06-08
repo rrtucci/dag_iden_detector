0ec897040a7a2ca08a6a7eca36a3728ff543b0a0
rrtucci <tucci@ar-tiste.com>
2026-06-08 06:04:36 -0400

    first commit

--- adjustment_formulae.py
+++ adjustment_formulae.py
@@ -38,7 +38,7 @@ def get_frontdoor_adjustment_prob(bnet, full_pot):
     return prob_y_bar_x
 
 
-def get_napkin_adjustment_prob(bnet, full_pot):
+def get_napkin1_adjustment_prob(bnet, full_pot):
     nd_w = bnet.get_node_named('w')
     nd_z = bnet.get_node_named('z')
     nd_x = bnet.get_node_named('x')
@@ -61,3 +61,98 @@ def get_napkin_adjustment_prob(bnet, full_pot):
     pot_y_bar_x.normalize_self()
     prob_y_bar_x = pot_y_bar_x.pot_arr
     return prob_y_bar_x
+
+def get_napkin2_adjustment_prob(bnet, full_pot):
+    nd_w = bnet.get_node_named('w')
+    nd_x = bnet.get_node_named('x')
+    nd_y = bnet.get_node_named('y')
+    pot_wxy = full_pot.get_new_marginal([nd_w, nd_x, nd_y])
+    pot_wx = full_pot.get_new_marginal([nd_w, nd_x])
+    pot_x = pot_wx.get_new_marginal([nd_x])
+
+    pot_wy = (pot_wxy * pot_x / pot_wx).get_new_marginal([nd_w, nd_y])
+    pot_xy = (pot_wy * pot_wx / pot_x).get_new_marginal([nd_x, nd_y])
+    arr_xy = pot_xy.pot_arr
+    pot_y_bar_x = DiscreteCondPot(False,
+                                  [nd_x, nd_y],
+                                  arr_xy)
+    pot_y_bar_x.normalize_self()
+    prob_y_bar_x = pot_y_bar_x.pot_arr
+    return prob_y_bar_x
+
+def get_napkin3_adjustment_prob(bnet, full_pot):
+    nd_z = bnet.get_node_named('z')
+    nd_x = bnet.get_node_named('x')
+    nd_y = bnet.get_node_named('y')
+    pot_zxy = full_pot.get_new_marginal([nd_z, nd_x, nd_y])
+    pot_zx = full_pot.get_new_marginal([nd_z, nd_x])
+    pot_x = pot_zx.get_new_marginal([nd_x])
+
+    pot_zy = (pot_zxy * pot_x / pot_zx).get_new_marginal([nd_z, nd_y])
+    pot_xy = (pot_zy * pot_zx / pot_x).get_new_marginal([nd_x, nd_y])
+    arr_xy = pot_xy.pot_arr
+    pot_y_bar_x = DiscreteCondPot(False,
+                                  [nd_x, nd_y],
+                                  arr_xy)
+    pot_y_bar_x.normalize_self()
+    prob_y_bar_x = pot_y_bar_x.pot_arr
+    return prob_y_bar_x
+
+def get_napkin4_adjustment_prob(bnet, full_pot):
+    nd_z = bnet.get_node_named('z')
+    nd_x = bnet.get_node_named('x')
+    nd_y = bnet.get_node_named('y')
+
+    pot_zxy = full_pot.get_new_marginal([nd_z, nd_x, nd_y])
+    pot_zx = pot_zxy.get_new_marginal([nd_z, nd_x])
+    pot_z = pot_zx.get_new_marginal([nd_z])
+
+    final_pot = (pot_zxy/pot_zx)*pot_z
+
+    arr_y_bar_x = final_pot.get_new_marginal([nd_x, nd_y]).pot_arr
+    pot_y_bar_x = DiscreteCondPot(False,
+                                  [nd_x, nd_y],
+                                  arr_y_bar_x)
+    pot_y_bar_x.normalize_self()
+    prob_y_bar_x = pot_y_bar_x.pot_arr
+    return prob_y_bar_x
+
+def get_napkin5_adjustment_prob(bnet, full_pot):
+    nd_w = bnet.get_node_named('w')
+    nd_x = bnet.get_node_named('x')
+    nd_y = bnet.get_node_named('y')
+
+    pot_wxy = full_pot.get_new_marginal([nd_w, nd_x, nd_y])
+    pot_wx = pot_wxy.get_new_marginal([nd_w, nd_x])
+    pot_w = pot_wx.get_new_marginal([nd_w])
+
+    final_pot = (pot_wxy/pot_wx)*pot_w
+
+    arr_y_bar_x = final_pot.get_new_marginal([nd_x, nd_y]).pot_arr
+    pot_y_bar_x = DiscreteCondPot(False,
+                                  [nd_x, nd_y],
+                                  arr_y_bar_x)
+    pot_y_bar_x.normalize_self()
+    prob_y_bar_x = pot_y_bar_x.pot_arr
+    return prob_y_bar_x
+
+def get_napkin6_adjustment_prob(bnet, full_pot):
+    nd_w = bnet.get_node_named('w')
+    nd_z = bnet.get_node_named('z')
+    nd_x = bnet.get_node_named('x')
+    nd_y = bnet.get_node_named('y')
+
+    pot_wzxy = full_pot.get_new_marginal([nd_w, nd_z, nd_x, nd_y])
+    pot_zw = pot_wzxy.get_new_marginal([nd_z, nd_w])
+    pot_w  = pot_zw.get_new_marginal([nd_w])
+    # print("nnmf", pot_w.pot_arr)
+
+    final_pot = (pot_wzxy/pot_zw)*pot_w
+
+    arr_y_bar_x_z = final_pot.get_new_marginal([nd_x, nd_z, nd_y]).pot_arr
+    pot_y_bar_x_z = DiscreteCondPot(False,
+                                  [nd_x, nd_z, nd_y],
+                                  arr_y_bar_x_z)
+    pot_y_bar_x_z.normalize_self()
+    prob_y_bar_x_z = pot_y_bar_x_z.pot_arr
+    return prob_y_bar_x_z
--- identification.py
+++ identification.py
@@ -78,7 +78,7 @@ def get_default_nd_to_size(dot_file, hidden_nd_names):
     return nd_to_size
 
 
-def calculate_ampu_and_full_pots(bnet):
+def calc_ampu_and_full_pots(bnet):
     node_list = list(bnet.nodes)
 
     nd_x = bnet.get_node_named('x')
@@ -95,15 +95,21 @@ def calculate_ampu_and_full_pots(bnet):
     return ampu_pot, full_pot
 
 
-def calculate_do_query_probs(bnet, ampu_pot, full_pot):
+def calc_ampu_and_full_prob_y_bar_x(bnet,
+                                    ampu_pot,
+                                    full_pot):
     nd_x = bnet.get_node_named('x')
     nd_y = bnet.get_node_named('y')
     ampu_prob_y_bar_x = None
     full_prob_y_bar_x = None
     for final_pot in [ampu_pot, full_pot]:
-        arr_xy = final_pot.get_new_marginal([nd_x, nd_y]).pot_arr
+        arr_xy = final_pot.get_new_marginal(
+            [nd_x, nd_y]).pot_arr
         pot_y_bar_x = DiscreteCondPot(
-            False, [nd_x, nd_y], arr_xy)
+            False,
+            [nd_x, nd_y],
+            arr_xy)
+
         pot_y_bar_x.normalize_self()
         if final_pot == ampu_pot:
             ampu_prob_y_bar_x = pot_y_bar_x.pot_arr
@@ -115,10 +121,12 @@ def calculate_do_query_probs(bnet, ampu_pot, full_pot):
     return ampu_prob_y_bar_x, full_prob_y_bar_x
 
 
-def compare_two_do_queries(dot_file,
+def print_all_prob_y_bar_x(dot_file,
                            hidden_nd_names,
                            nd_to_size=None,
-                           verbose=True):
+                           verbose=True,
+                           adj_version=1,
+                           num_bnet_samples=2):
     if not nd_to_size:
         nd_to_size = get_default_nd_to_size(dot_file, hidden_nd_names)
     nodes, arrows = DotTool.read_dot_file(dot_file)
@@ -128,30 +136,40 @@ def compare_two_do_queries(dot_file,
         arrows,
         nd_to_size)
 
-    for bnet_str in ["bnet1", "bnet2"]:
+    if num_bnet_samples == 1:
+        bnet_strings = ["bnet1"]
+    else:
+        bnet_strings = ["bnet1", "bnet2"]
+    for bnet_str in bnet_strings:
         if bnet_str == "bnet2":
             print("------------------------------")
             randomize_these_nodes(bnet, hidden_nd_names)
         print(f"Random {bnet_str}:")
         if verbose:
             print(bnet)
-        ampu_pot, full_pot = calculate_ampu_and_full_pots(bnet)
+        ampu_pot, full_pot = calc_ampu_and_full_pots(bnet)
         ampu_prob_y_bar_x, full_prob_y_bar_x = \
-            calculate_do_query_probs(bnet, ampu_pot, full_pot)
+            calc_ampu_and_full_prob_y_bar_x(bnet, ampu_pot, full_pot)
         print()
         print(f"full P(y|x) for {bnet_str}:")
         pprint(full_prob_y_bar_x)
         print()
         print(f"amputated P(y|x) for {bnet_str}: (REQUIRES RCT)")
         pprint(ampu_prob_y_bar_x)
-        dotf_str_to_adj_method = {
-            "back-door": get_backdoor_adjustment_prob,
-            "front-door": get_frontdoor_adjustment_prob,
-            "napkin": get_napkin_adjustment_prob}
+        dotf_strings = ["back-door", "front-door", "napkin"]
+        adj_id_to_adj_method = {
+            "back-door1": get_backdoor_adjustment_prob,
+            "front-door1": get_frontdoor_adjustment_prob,
+            "napkin1": get_napkin1_adjustment_prob,
+            "napkin2": get_napkin2_adjustment_prob,
+            "napkin3": get_napkin3_adjustment_prob,
+            "napkin4": get_napkin4_adjustment_prob,
+            "napkin5": get_napkin5_adjustment_prob}
         adj_method = None
-        for dotf_str in dotf_str_to_adj_method:
+        for dotf_str in dotf_strings:
             if dotf_str in dot_file:
-                adj_method = dotf_str_to_adj_method[dotf_str]
+                adj_id = dotf_str + str(adj_version)
+                adj_method = adj_id_to_adj_method[adj_id]
                 break
         if adj_method:
             print()
@@ -163,25 +181,155 @@ def compare_two_do_queries(dot_file,
             pprint(adj_method(bnet, ampu_pot))
 
 
+def calc_ampu_and_full_prob_y_bar_x_z(bnet,
+                                      ampu_pot,
+                                      full_pot,
+                                      other_cond):
+    assert isinstance(other_cond, str)
+    nd_x = bnet.get_node_named('x')
+    nd_y = bnet.get_node_named('y')
+    nd_z = bnet.get_node_named(other_cond)
+    ampu_prob_y_bar_x_z = None
+    full_prob_y_bar_x_z = None
+    for final_pot in [ampu_pot, full_pot]:
+        arr_zxy = final_pot.get_new_marginal(
+            [nd_z, nd_x, nd_y]).pot_arr
+        pot_y_bar_x_z = DiscreteCondPot(
+            False,
+            [nd_z, nd_x, nd_y],
+            arr_zxy)
+
+        pot_y_bar_x_z.normalize_self()
+        if final_pot == ampu_pot:
+            ampu_prob_y_bar_x_z = pot_y_bar_x_z.pot_arr
+        elif final_pot == full_pot:
+            full_prob_y_bar_x_z = pot_y_bar_x_z.pot_arr
+        else:
+            assert None
+
+    return ampu_prob_y_bar_x_z, full_prob_y_bar_x_z
+
+
+def print_all_prob_y_bar_x_z(dot_file,
+                             hidden_nd_names,
+                             other_cond,
+                             nd_to_size=None,
+                             verbose=True,
+                             adj_version=1,
+                             num_bnet_samples=2):
+    assert isinstance(other_cond, str)
+    if not nd_to_size:
+        nd_to_size = get_default_nd_to_size(dot_file, hidden_nd_names)
+    nodes, arrows = DotTool.read_dot_file(dot_file)
+
+    bnet = create_random_bnet(
+        nodes,
+        arrows,
+        nd_to_size)
+
+    if num_bnet_samples == 1:
+        bnet_strings = ["bnet1"]
+    else:
+        bnet_strings = ["bnet1", "bnet2"]
+    for bnet_str in bnet_strings:
+        if bnet_str == "bnet2":
+            print("------------------------------")
+            randomize_these_nodes(bnet, hidden_nd_names)
+        print(f"Random {bnet_str}:")
+        if verbose:
+            print(bnet)
+        ampu_pot, full_pot = calc_ampu_and_full_pots(bnet)
+        ampu_prob_y_bar_x_z, full_prob_y_bar_x_z = \
+            calc_ampu_and_full_prob_y_bar_x_z(
+                bnet, ampu_pot, full_pot, other_cond)
+        print()
+        print(f"full P(y|x, z) for {bnet_str}:")
+        pprint(full_prob_y_bar_x_z)
+        print()
+        print(f"amputated P(y|x, z) for {bnet_str}: (REQUIRES RCT)")
+        pprint(ampu_prob_y_bar_x_z)
+        dotf_strings = ["napkin"]
+        adj_id_to_adj_method = {
+            "napkin6": get_napkin6_adjustment_prob}
+        adj_method = None
+        for dotf_str in dotf_strings:
+            if dotf_str in dot_file:
+                adj_id = dotf_str + str(adj_version)
+                adj_method = adj_id_to_adj_method[adj_id]
+                break
+        if adj_method:
+            print()
+            print(f"adjusted P(y|x, "
+                  f"{other_cond}) from full_pot for {bnet_str}:")
+            pprint(adj_method(bnet, full_pot))
+            print()
+            print(f"adjusted P(y|x, "
+                  f"{other_cond}) from ampu_pot for {bnet_str}:"
+                  f"(REQUIRES RCT)")
+            pprint(adj_method(bnet, ampu_pot))
+
+
 if __name__ == "__main__":
     def main_backdoor(draw, verbose):
-        compare_two_do_queries(dot_file="dot_atlas/back-door.dot",
+        print_all_prob_y_bar_x(dot_file="dot_atlas/back-door.dot",
                                hidden_nd_names=[],
                                verbose=verbose)
 
 
     def main_frontdoor(draw, verbose):
-        compare_two_do_queries(dot_file="dot_atlas/front-door.dot",
+        print_all_prob_y_bar_x(dot_file="dot_atlas/front-door.dot",
                                hidden_nd_names=["h"],
                                verbose=verbose)
 
 
-    def main_napkin(draw, verbose):
-        compare_two_do_queries(dot_file="dot_atlas/napkin.dot",
+    def main_napkin1(draw, verbose):
+        print_all_prob_y_bar_x(dot_file="dot_atlas/napkin.dot",
                                hidden_nd_names=["u_1", "u_2"],
+                               adj_version=1,
                                verbose=verbose)
+
+
+    def main_napkin2(draw, verbose):
+        print_all_prob_y_bar_x(dot_file="dot_atlas/napkin.dot",
+                               hidden_nd_names=["u_1", "u_2"],
+                               adj_version=2,
+                               verbose=verbose)
+
+
+    def main_napkin3(draw, verbose):
+        print_all_prob_y_bar_x(dot_file="dot_atlas/napkin.dot",
+                               hidden_nd_names=["u_1", "u_2"],
+                               adj_version=3,
+                               verbose=verbose)
+
+
+    def main_napkin4(draw, verbose):
+        print_all_prob_y_bar_x(dot_file="dot_atlas/napkin.dot",
+                               hidden_nd_names=["u_1", "u_2"],
+                               adj_version=4,
+                               verbose=verbose)
+
+
+    def main_napkin5(draw, verbose):
+        print_all_prob_y_bar_x(dot_file="dot_atlas/napkin.dot",
+                               hidden_nd_names=["u_1", "u_2"],
+                               adj_version=5,
+                               verbose=verbose)
+
+    def main_napkin6(draw, verbose):
+        print_all_prob_y_bar_x_z(dot_file="dot_atlas/napkin.dot",
+                               hidden_nd_names=["u_1", "u_2"],
+                               other_cond = "z",
+                               adj_version=6,
+                               verbose=verbose)
+
 
 
     # main_backdoor(False, False)
     # main_frontdoor(False, False)
-    main_napkin(False, False)
+    # main_napkin1(False, False)
+    # main_napkin2(False, False)
+    # main_napkin3(False, False)
+    # main_napkin4(False, False)
+    # main_napkin5(False, False)
+    main_napkin6(False, False)
